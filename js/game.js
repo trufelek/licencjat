b5f1018724f4d91e736ae8c3cac563987a636710
Aleksandra Wierzbiak <aleksandra.wierzbiak@extremelab.pl>
2015-06-11 18:43:43 +0200

    Małe poprawki w planszy. Życia bohatera. Serduszka uzdrawiające jeśli bohater ma mniej niż 3 życia.

--- js/game.js
+++ js/game.js
@@ -7,11 +7,12 @@ game = {
 	fall: false,
 	climb: false,
 	platform: false,
+	//funkcja inicjująca
 	init: function(){
-		//tworzy plansze
 		$('body').height(window.innerHeight - 50);
 		$('#game').append('<div id="board"></div>');
 		$('#game').append('<div id="score">Punkty: <span id="points">0</span></div>');
+		$('#game').append('<div id="lives"><span id="hearts" class="three"></span></div>');
 		$('#board').append('<div class="background"></div>');
 
 		//rysuje mape kafelków
@@ -20,11 +21,13 @@ game = {
 		//dodaje bohatera
 		$('#board').append(player.div);
 	},
-	intersect: function(a1,a2,b1,b2){
-  		var i1 = Math.min(Math.max(a1, b1), a2);
-    	var i2 = Math.max(Math.min(a2, b2), a1);
+	//funkcja znajduje część wspólną
+	intersect: function(x1,x2,y1,y2){
+  		var i1 = Math.min(Math.max(x1, y1), x2);
+    	var i2 = Math.max(Math.min(x2, y2), x1);
     	return [i1, i2];
 	}, 
+	//detekcja kolizji
 	collide: function(player_x, player_y, player_w, player_h){
 		game.collisions = [];
 		$('.tile').each(function(i, tile){
@@ -34,7 +37,20 @@ game = {
 			var tile_h = $(tile).height();
 
 			if((player_x + player_w > tile_x) && (player_x < tile_x + tile_w) && (player_y + player_h > tile_y) && (player_y < tile_y + tile_h)){
-				game.collisions.push(tile);
+				if($(tile).hasClass('diamonds')){
+					console.log('diamond!');
+					player.loot(tile);
+				}else if($(tile).hasClass('heart')){
+					console.log(player.lives);
+					if(player.lives < 3){
+						player.heal(tile);
+					}
+				}else if($(tile).hasClass('spikes')){
+					console.log('spikes!');
+					player.status = "dead";
+				}else{
+					game.collisions.push(tile);
+				}
 			}
 
 
@@ -52,17 +68,6 @@ game = {
 					console.log('collide!');
 					console.log(tile);
 					return game.collisions;
-				}
-			}else if((player.x + player.w > tile_x) && (player.x < tile_x + tile_w) && (player.y + player.h >= tile_y) && (player.y <= tile_y + tile_h)){
-				if($(tile).hasClass('ladder')){
-					game.climb = true;
-				}else if($(tile).hasClass('platform')){
-					game.platform = true;
-				}
-			}else if((player.x + player.w > tile_x) && (player.x < tile_x + tile_w) && (player.y +  player.h < tile_y)){
-				if(player.status != 'climbing'){
-					game.fall = true;
-					return false;
 				}
 			}
 			*/
--- js/player.js
+++ js/player.js
@@ -5,80 +5,98 @@ player = {
     status : "stand",
     motion : 0,
     delta: 30,
+    lives: 3,
 	update: function(){
-        player.speed = Math.min(100,Math.max(-100, player.speed + player.acceleration * player.delta / 100.0)); 
-        var x = player.div.position().left + player.motion;
-        var y = player.div.position().top + player.speed * player.delta / 100.0;
-        var w = player.div.width();
-        var h = player.div.height();   
+		if(player.status == "dead"){
+			player.die();
+			player.status = "stand";
+			player.div.css('top', 460 + 'px').css('left', 0 + 'px');
+			player.div.fadeOut('slow').fadeIn('slow');
+		}else{
+			player.speed = Math.min(100,Math.max(-100, player.speed + player.acceleration * player.delta / 100.0)); 
+	        var x = player.div.position().left + player.motion;
+	        var y = player.div.position().top + player.speed * player.delta / 100.0;
+	        var w = player.div.width();
+	        var h = player.div.height();   
 
-        //detekcja kolizji powinna zwrócić kafelki z którymi koliduje postać
-       	var collisions = game.collide(x, y, w, h);
-       	var i = 0;
-        while (i < collisions.length > 0) {
-            var collision = collisions[i];
-            i++;
-            var collisionBox = {
-                x1: $(collision).position().left,
-                y1: $(collision).position().top,
-                x2: $(collision).position().left + $(collision).width(),
-                y2: $(collision).position().top + $(collision).height()
-            };
-            
-            var xx = game.intersect(x, x + w, collisionBox.x1,collisionBox.x2);
-            var yy = game.intersect(y, y + h, collisionBox.y1,collisionBox.y2);
-            
-            var diffx = (xx[0] === x)? xx[0]-xx[1] : xx[1]-xx[0];
-            var diffy = (yy[0] === y)? yy[0]-yy[1] : yy[1]-yy[0];
-            if (Math.abs(diffx) > Math.abs(diffy)){
-                // przesunięcie na osi Y
-                 y -= diffy;
-                 player.speed = 0;
-                 if(player.status =="jump" && diffy > 0){
-                    player.status="stand";
-                    // gf.setAnimation(this.div, playerAnim.stand);
-                 }
-            } else {
-                // przesunięcie na osi X
-                y -= diffx;
-            }
-        }
-      
-      	player.div.css('left', x);
-      	player.div.css('top', y);
-      	player.motion = 0;
+	        //detekcja kolizji powinna zwrócić kafelki z którymi koliduje postać
+	       	var collisions = game.collide(x, y, w, h);
+	       	var i = 0;
+	        while (i < collisions.length > 0) {
+	            var collision = collisions[i];
+	            i++;
+	            var collisionBox = {
+	                x1: $(collision).position().left,
+	                y1: $(collision).position().top,
+	                x2: $(collision).position().left + $(collision).width(),
+	                y2: $(collision).position().top + $(collision).height()
+	            };
+	            
+	            var xx = game.intersect(x, x + w, collisionBox.x1,collisionBox.x2);
+	            var yy = game.intersect(y, y + h, collisionBox.y1,collisionBox.y2);
+	            
+	            var diffx = (xx[0] === x)? xx[0]-xx[1] : xx[1]-xx[0];
+	            var diffy = (yy[0] === y)? yy[0]-yy[1] : yy[1]-yy[0];
+	            if (Math.abs(diffx) > Math.abs(diffy)){
+	                // przesunięcie na osi Y
+	                 y -= diffy;
+	                 player.speed = 0;
+	                 if(player.status =="jump" && diffy > 0){
+	                    player.status="stand";
+	                    // gf.setAnimation(this.div, playerAnim.stand);
+	                 }
+	            } else {
+	                // przesunięcie na osi X
+	                y -= diffx;
+	            }
+	        }
+	      
+	      	player.div.css('left', x);
+	      	player.div.css('top', y);
+	      	player.motion = 0;
+		}
 	},
 	move: function(action){
 		switch(action){
 			//ruch w lewo
 			case 'left':{
 				if(player.status != "climbing"){
-	 				if(player.status == "stand"){
-		            	//game.animate(player.div, walk, true);
-		            	status = "walk";
-		            	player.motion -= 10;
-		            }else if(player.status == "jump"){
-		            	player.motion -= 5;
-		            }else if(player.status == "walk"){
-		            	player.motion -= 10;
-		            }
+					if(player.div.position().left > 400 && $('div.first').position().left < 0){
+			   			$('.tile').css('left', '+=5px');
+			   		}else{
+			   			if(player.status == "stand"){
+		            		//game.animate(player.div, walk, true);
+		            		status = "walk";
+		            		player.motion -= 10;
+			            }else if(player.status == "jump"){
+			            	player.motion -= 5;
+			            }else if(player.status == "walk"){
+			            	player.motion -= 10;
+			            }
+			   		}	
 		            //game.transform(player.div);
+		            player.div.css('background-position', '0px 0px');
 				}
 			}
 			break;
 			//ruch w prawo
 			case 'right':{
 				if(player.status != "climbing"){
-	 				if(player.status == "stand"){
-		            	//game.animate(player.div, walk, true);
-		            	status = "walk";
-		            	player.motion += 10;
-		            }else if(player.status == "jump"){
-		            	player.motion += 5;
-		            }else if(player.status == "walk"){
-		            	player.motion += 10;
-		            }
-		            //game.transform(player.div);
+					if(player.div.position().left > 400 && $('div.last').position().left > 720){
+						$('.tile').css('left', '-=5px');
+					}else{
+						if(player.status == "stand"){
+			            	//game.animate(player.div, walk, true);
+			            	status = "walk";
+			            	player.motion += 10;
+			            }else if(player.status == "jump"){
+			            	player.motion += 5;
+			            }else if(player.status == "walk"){
+			            	player.motion += 10;
+			            }
+					}
+					//game.transform(player.div);
+			        player.div.css('background-position', '0px -20px');
 				}
 			}
 			break;
@@ -117,8 +135,29 @@ player = {
 		game.points += 1;
 		$('#points').text(game.points);
 	},
+	heal: function(heart){
+		$(heart).remove();
+		player.lives ++;
+
+		if(player.lives == 3){
+			$('#hearts').removeClass();
+			$('#hearts').addClass('three');
+		}else if(player.lives == 2){
+			$('#hearts').removeClass();
+			$('#hearts').addClass('two');
+		}
+	},
 	die: function(){
-		player.div.fadeOut('slow');
+		player.lives --;
+		if(player.lives == 2){
+			$('#hearts').removeClass();
+			$('#hearts').addClass('two');
+		}else if(player.lives == 1){
+			$('#hearts').removeClass();
+			$('#hearts').addClass('one');
+		}else{
+			console.log('game over');
+		}
 	},
 	animate: function(direction){
 		switch(direction){
